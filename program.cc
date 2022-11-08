187ff855fdd5923f032c8ed3437f88269fa415b5
polipolo55 <pol.casanovas.puig@gmail.com>
2022-11-08 10:08:48 +0100

    afegit cin de la commanda i arreglat falta en el commit

--- program.cc
+++ program.cc
@@ -1,16 +1,18 @@
 /**
  * @mainpage Encriptación de mensajes.
- * Programa modular para la encriptación de mensajes mediante metodos de substitución o permutación 
- * usando las clases <em>Alfabets<em>, <em>Encriptacions<em>, y <em>Missatges<em>
-*/
+ * Programa modular para la encriptación de mensajes mediante metodos de
+ * substitución o permutación usando las clases <em>Alfabets<em>,
+ * <em>Encriptacions<em>, y <em>Missatges<em>
+ */
 
 /**
  * @file program.cc
  * @author Pol Casanovas Puig (pol.casanovas.puig@estudiantat.upc.edu)
- * @brief La parte principal de programa con la gestión de la input de los comandos
- * @version 0.1 
+ * @brief La parte principal de programa con la gestión de la input de los
+ * comandos
+ * @version 0.1
  * @date 07-11-22
- * 
+ *
  */
 
 #include "Alfabets.hh"
@@ -21,7 +23,7 @@ using namespace std;
 
 /**
  * @brief Parte principal de programa
- * 
+ *
  * @return main, no tiene return
  */
 int main() {
@@ -44,19 +46,22 @@ int main() {
       if (alf.is_alf_there(ida)) {
         string idm;
         cin >> idm;
-        if(!msg.is_msg_there(idm)){
+        if (!msg.is_msg_there(idm)) {
           msg.add_msg(idm);
           cout << msg.n_msg() << endl;
-        } else cout << "ja existeix el missatge" << idm << endl;
-      } else cout << "no existeix l'alfabet: " << ida << endl;
-    
+        } else
+          cout << "ja existeix el missatge" << idm << endl;
+      } else
+        cout << "no existeix l'alfabet: " << ida << endl;
+
     } else if (s == "nuevo_alfabeto" or s == "na") {
       string ida;
       cin >> ida;
       if (!alf.is_alf_there(ida)) {
         alf.add_alf(ida);
         cout << alf.n_alf() << endl;
-      } else cout << "ja existeix l'alfabet: " << ida << endl;
+      } else
+        cout << "ja existeix l'alfabet: " << ida << endl;
 
     } else if (s == "borra_mensaje" or s == "bm") {
       string idm;
@@ -64,16 +69,19 @@ int main() {
       if (msg.is_msg_there(idm)) {
         msg.del_msg(idm);
         cout << msg.n_msg() << endl;
-      } else cout << "el missatge " << idm << " no existeix" << endl;
+      } else
+        cout << "el missatge " << idm << " no existeix" << endl;
 
     } else if (s == "borra_alfabeto" or s == "ba") {
       string ida;
       cin >> ida;
       if (alf.is_alf_there(ida)) {
-        if (1 > alf.n_of_msg(ida)){
+        if (1 > alf.n_of_msg(ida)) {
           alf.del_alf(ida);
-        } else cout << "l'alfabet " << ida << " conté missatges" << endl; 
-      } else cout << "no existeix l'alfabet " << ida << endl; 
+        } else
+          cout << "l'alfabet " << ida << " conté missatges" << endl;
+      } else
+        cout << "no existeix l'alfabet " << ida << endl;
 
     } else if (s == "listar_mensajes" or s == "lm") {
       msg.print_missatges();
@@ -88,18 +96,20 @@ int main() {
         string cl;
         cin >> cl;
         enc.encrip_sust(msg.get_alf(idm), msg.get_msg(idm), cl);
-      } else cout << "el missatge " << idm << " no existeix" << endl;
+      } else
+        cout << "el missatge " << idm << " no existeix" << endl;
 
     } else if (s == "codificar_sustitucion" or s == "cs") {
       string ida, cl, msg;
       cin >> ida;
-      if (alf.is_alf_there(ida)){
+      if (alf.is_alf_there(ida)) {
         cin >> cl >> msg;
         enc.encrip_sust(ida, msg, cl);
-      } else cout << "l'alfabet " << ida << " no existeix" << endl; 
+      } else
+        cout << "l'alfabet " << ida << " no existeix" << endl;
 
     } else if (s == "decodificar_sustitución" or s == "ds") {
-        //mirar com fer es pot fer al reves depenent del algoritme
+      // mirar com fer es pot fer al reves depenent del algoritme
     } else if (s == "codificar_permutacion_guardado" or s == "cpg") {
       string idm;
       cin >> idm;
@@ -107,19 +117,22 @@ int main() {
         int b;
         cin >> b;
         enc.encrip_perm(msg.get_alf(idm), msg.get_msg(idm), b);
-      } else cout << "el missatge " << idm << " no existeix" << endl;
+      } else
+        cout << "el missatge " << idm << " no existeix" << endl;
 
     } else if (s == "codificar_permutacion" or s == "cp") {
       string ida, msg;
       int b;
       cin >> ida;
-      if (alf.is_alf_there(ida)){
+      if (alf.is_alf_there(ida)) {
         cin >> b >> msg;
         enc.encrip_perm(ida, msg, b);
-      } else cout << "l'alfabet " << ida << " no existeix" << endl; 
- 
+      } else
+        cout << "l'alfabet " << ida << " no existeix" << endl;
+
     } else if (s == "decodificar_permutacion" or s == "dp") {
-      //mirar com fer-ho un cop pensat l'algoritme 
+      // mirar com fer-ho un cop pensat l'algoritme
     }
+    cin >> s;
   }
 }
