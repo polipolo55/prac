91393fa6813036b3077306a674bfb40d018cbf31
polipolo55 <pol.casanovas.puig@gmail.com>
2022-11-22 17:03:32 +0100

    before mod.

---
+++ Encriptacions.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "Alfabets.hh"
+#include "Alfabet.hh"
+#include "Encriptacions.hh"
+using namespace std;
+
+
+Encriptacions::Encriptacions() {}
+
+void Encriptacions::encrip_sust(const Alfabet& alf,const string& msg, const string& cl) {
+
+}
+
+void Encriptacions::encrip_perm(const string& ida,const string& msg, const int& b) {
+
+}
+
+
+
